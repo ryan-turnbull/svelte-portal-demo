f02bf673a3e46279638d1e63210b5ff2eb955350
Ryan turnbull <ryanturnbullemail@gmail.com>
2019-07-07 20:56:53 +1200

    feat(routing): add routes and router package with basic landing

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script>
-	export let name;
+	import { Router, Link, Route } from "svelte-routing";
+	import * as routes from './constants/routes'
+
+	import Landing from './pages/Landing.svelte'
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
+<Router url="">
+	<Route path={routes.LANDING}><Landing /></Route>
+</Router>
 
-<h1>Hello {name}!</h1>
---
+++ src/constants/routes.js
@@ -0,0 +1,7 @@
+export const LANDING = '/'
+export const LOGIN = '/login'
+export const RESET_PASSWORD = '/reset'
+
+export const HOME = '/dashboard/home'
+export const USERS = '/dashboard/users'
+export const SETTINGS = '/dashboard/settings'
